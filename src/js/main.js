6ac6b44b9558cc0a9a2011276b6cb7a30617aa75
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-03 15:44:36 -0400

    wrote put the whote FizzBuzz function

--- src/js/main.js
+++ src/js/main.js
@@ -11,17 +11,17 @@ function fizz (number) {
 };
 
 function fizzBuzz (start, end) {
-  // for (var count = start; count <= end; count++) {
-  //   if (count % 15 === 0) {
-  //     return ("FizzBuzz");
-  //   } else if (count % 5 === 0) {
-  //     return ("Buzz");
-  //   } else if (count % 3 === 0) {
-  //     return ("Fizz");
-  //   } else {
-  //     return (count)
-  //   }
-  // }
+  for (var count = start; count <= end; count++) {
+    if (count % 15 === 0) {
+      return ("FizzBuzz");
+    } else if (count % 5 === 0) {
+      return ("Buzz");
+    } else if (count % 3 === 0) {
+      return ("Fizz");
+    } else {
+      return (count)
+    }
+  }
 };
 
 export { fizz };
