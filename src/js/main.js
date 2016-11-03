1061e2a460c892eb91885b90677cd858bdec615d
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-03 15:42:35 -0400

    got my final test to pass

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 function fizz (number) {
-  if (number % 5 === 0) {
-    return ("Buzz")
+  if (number % 15 === 0) {
+    return ("FizzBuzz");
+  } else if (number % 5 === 0) {
+    return ("Buzz");
   } else if (number % 3 === 0){
     return ("Fizz");
   } else {
