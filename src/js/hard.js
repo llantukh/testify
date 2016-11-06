46360eb96d593ce798d29803b01b993184925405
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-06 15:06:29 -0500

    got all tests passing except for two on the brie. i am a little confused.

--- src/js/hard.js
+++ src/js/hard.js
@@ -11,7 +11,7 @@ class GildedRose {
     } else if (this.name.includes("Sulfuras", "Hand of Ragnaros")) {
       this.legendary();
     } else if (this.name.includes("Backstage")) {
-      this.backStage();
+      this.concert();
     } else if (this.name.includes("Conjured")) {
       this.conjured();
     } else {
@@ -22,19 +22,46 @@ class GildedRose {
   agedBrie () {
     this.sellIn--;
     this.quality++;
+    // if (this.sellIn <= 0) {
+    //   this. quality = this. quality + 2;
+    // }else
     if (this.quality > 50) {
       this.quality = 50;
     }
-
   }
 
   legendary () {
     this.quality = 80;
   }
 
-  backStage () {}
+  concert () {
+    this.sellIn--;
+    if (this.sellIn < 0) {
+      this.quality = 0;
+    } else if (this.sellIn < 5) {
+      this.quality = this.quality + 3;
+    } else if (this.sellIn < 10) {
+      this.quality = this.quality + 2;
+    } else {
+      this.quality++;
+    }
+    if (this.quality > 50){
+      this.quality = 50;
+    }
+  }
 
-  conjured () {}
+  conjured () {
+    this.sellIn--;
+    if (this.sellIn > 0) {
+      this.quality = this.quality - 2;
+    }
+    if (this.sellIn < 0) {
+      this.quality = this.quality - 4;
+    }
+    if (this.quality < 0) {
+      this.quality = 0
+    }
+  }
 
   normal () {
     this.sellIn--;
